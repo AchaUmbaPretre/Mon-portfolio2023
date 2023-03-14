5bdd4a56541c7768e3f6c0a5ee6731b1b340b874
achaumba <achandambi@gmail.com>
2023-03-14 09:55:06 -1100

    4ieme commit

--- fonts/js/script.js
+++ fonts/js/script.js
@@ -2,5 +2,5 @@ const headerUl = document.querySelector(".header-ul");
 const bars = document.querySelector(".bars");
 
 bars.addEventListener('click', ()=>{
-    headerUl.style.display = "block";
+    headerUl.classList.toggle('open')
 })
