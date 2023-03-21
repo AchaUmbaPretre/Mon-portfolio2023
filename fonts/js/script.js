4abaef4f3805d5c329c91a044838e3781367fc8e
achaumba <achandambi@gmail.com>
2023-03-21 04:53:59 -1100

    6ieme

--- fonts/js/script.js
+++ fonts/js/script.js
@@ -4,3 +4,22 @@ const bars = document.querySelector(".bars");
 bars.addEventListener('click', ()=>{
     headerUl.classList.toggle('open')
 })
+
+
+//scrollreveal
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.title-h1`, {origin:'left'})
+sr.reveal(`.title-h11`, {origin:'right'})
+sr.reveal(`.slider-desc`, {origin:'left'})
+sr.reveal(`.btn`, {origin:'right'})
+sr.reveal(`.apropos-left`, {delay: 800, origin: 'left', interval: 100})
+sr.reveal(`.apropos-right`, {delay: 800, origin: 'right', interval: 100})
+sr.reveal(`.about-row`, {delay: 300, origin: 'bottom', interval: 100})
+sr.reveal(`.container-form`, {origin:'bottom'})
